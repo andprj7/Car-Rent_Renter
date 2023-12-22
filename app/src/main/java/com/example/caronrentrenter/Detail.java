36c968c9600d4979e704a1b26ee79e5a1fe81a8d
RAMANI MEET <meetgramani6697@gmail.com>
2023-12-22 14:30:10 +0530

    MEET RAMANI

--- app/src/main/java/com/example/caronrentrenter/Detail.java
+++ app/src/main/java/com/example/caronrentrenter/Detail.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,8 +40,10 @@ public class Detail extends AppCompatActivity {
 
     String rmob,mobiii,mob1, imageUrl;
     private ReadWriteUserDetails object1;
+    Button submit;
     TextView titleTxt,txtRating,txtPassengers,txtGear,txtMaxSpeed;
     TextView txtRname,txtMob;
+    ImageView call;
     String mob;
     String firebaseImageUrl;
     ImageView img,img_renter,img_favourite;
@@ -69,6 +72,8 @@ public class Detail extends AppCompatActivity {
         img = findViewById(R.id.img);
         img_renter = findViewById(R.id.img_renter);
         img_favourite = findViewById(R.id.img_favourite);
+        submit = findViewById(R.id.button);
+        call = findViewById(R.id.img_call);
 
 
         txtRname = findViewById(R.id.txtrname);
@@ -236,7 +241,20 @@ public class Detail extends AppCompatActivity {
             }
         });
 
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Detail.this, Date_Book.class));
+            }
+        });
 
 
 
